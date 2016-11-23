3754db36c11234697d016eb89e61f0d111908976
zaxlct <zaxlct@foxmail.com>
2016-11-23 10:03:41 +0800

    dev

--- js/control.js
+++ js/control.js
@@ -1,1 +1,90 @@
-$(function(){function n(){var n='<img class="down_sign hidden-xs" id="down_1" src="../img/down.png" />',e='<img class="down_sign hidden-xs" id="down_2" src="../img/down-2.png" />',s='<img class="down_sign hidden-xs" id="down_3" src="../img/up.png" />';$("#fullpage").fullpage({verticalCentered:!1,resize:!0,scrollingSpeed:700,loopBottom:!1,navigation:!0,navigationPosition:"left",fixedElements:"#logoFilxed",afterLoad:function(d,t){9===t||10===t?$("#logoFilxed").hide():$("#logoFilxed").show(),8===t&&($(".down_sign").remove(),$("#fp-nav").append(n)),9===t&&($(".down_sign").remove(),$("#fp-nav").append(e)),10===t&&($(".down_sign").remove(),$("#fp-nav").append(s),$),t<8&&$(".down_sign").remove(),t>9?$("#footer").show():$("#footer").hide(),9!==t&&o(),10===t&&$(window).width()<768&&i(),10!==t&&$("#desc_section").fadeIn()}})}function e(){$("#pic_list .pic").click(function(){var n=$(this).index("#pic_list  .pic")+1;$("#mask_msg").fadeIn().children(".pic").css({background:"url(../img/bg_small-"+n+".png) center center no-repeat",backgroundSize:"cover"}),$("#mask_msg .desc").html(textList[n-1])}),$("#close_mask").click(o)}function i(){setTimeout(function(){$("#desc_section").fadeOut()},2e3)}window.fnames=new Array,window.ftypes=new Array,fnames[0]="EMAIL",ftypes[0]="email",fnames[2]="NAME",ftypes[2]="text",fnames[3]="CITY",ftypes[3]="text",fnames[1]="DEVICE",ftypes[1]="radio";var o=function(){$("#mask_msg").fadeOut()};n(),e()});
+$(function() {
+  window.fnames = new Array();
+  window.ftypes = new Array();
+  fnames[0] = 'EMAIL';
+  ftypes[0] = 'email';
+  fnames[2] = 'NAME';
+  ftypes[2] = 'text';
+  fnames[3] = 'CITY';
+  ftypes[3] = 'text';
+  fnames[1] = 'DEVICE';
+  ftypes[1] = 'radio';
+
+
+  var closeMask = function() {
+    $('#mask_msg').fadeOut()
+  }
+
+  fullPageStart()
+  function fullPageStart() {
+    var str1 = '<img class="down_sign hidden-xs" id="down_1" src="../img/down.png" />'
+    var str2 = '<img class="down_sign hidden-xs" id="down_2" src="../img/down-2.png" />'
+    var str3 = '<img class="down_sign hidden-xs" id="down_3" src="../img/up.png" />'
+
+    $('#fullpage').fullpage({
+      verticalCentered: false,
+      resize: true,
+      scrollingSpeed: 700,
+      loopBottom: false,
+      navigation: true,
+      navigationPosition: 'left',
+      fixedElements: '#logoFilxed',
+      afterLoad: function(anchorLink, index) {
+        if(index === 9 || index === 10) {
+          $('#logoFilxed').hide()
+        } else {
+          $('#logoFilxed').show()
+        }
+
+        if(index === 8) {
+          $('.down_sign').remove()
+          $('#fp-nav').append(str1)
+        }
+
+        if(index === 9) {
+          $('.down_sign').remove()
+          $('#fp-nav').append(str2)
+        }
+
+        if(index === 10) {
+          $('.down_sign').remove()
+          $('#fp-nav').append(str3)
+          $
+        }
+
+        if(index < 8) {
+          $('.down_sign').remove()
+        }
+
+        if(index > 9) {
+          $('#footer').show()
+        } else {
+          $('#footer').hide()
+        }
+
+        if(index !== 9) closeMask()
+        if(index === 10 && $(window).width() < 768) descHide()
+        if(index !== 10 ) $('#desc_section').fadeIn()
+      },
+    })
+  }
+
+  maskFade()
+  function maskFade() {
+    $('#pic_list .pic').click(function() {
+      var index = $(this).index('#pic_list  .pic') + 1;
+      $('#mask_msg').fadeIn().children('.pic').css({
+        background: 'url(../img/bg_small-' + index + '.png) center center no-repeat',
+        backgroundSize: 'cover'
+      })
+
+      $('#mask_msg .desc').html(textList[index - 1])
+    })
+
+    $('#close_mask').click(closeMask)
+  }
+
+  function descHide(){
+    setTimeout(function() {$('#desc_section').fadeOut()}, 2000)
+  }
+})
